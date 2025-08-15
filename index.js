18976c646e74ff2a48ae0bfa74a54b297cd32d86
MrGalva07 <lucas.galvao0707@gmail.com>
2025-08-14 22:04:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Solidity types
+// bool,int,uint,fixed,ufixed
+
+
+
+//Convertendo para bits
+//8 bits equivalem a 1 byte 
+
+const decimal = 10
+//Método toString(2) converte para decimal na base 2 "010100"
+console.log(decimal.toString(2))
